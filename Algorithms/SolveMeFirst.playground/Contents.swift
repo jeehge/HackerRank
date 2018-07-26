5c6add188184dfd29333afb50d9f1de1b51b6114
jihye <taerg89@gmail.com>
2018-07-26 17:27:12 +0900

    Algorithms - Solve Me First

---
+++ Algorithms/SolveMeFirst.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var a = Int(readLine()!)!
+var b = Int(readLine()!)!
+
+// 입력한 a,b 값을 더하는 함수
+func solveMeFirst(x: Int, y: Int) -> Int {
+    return x+y
+}
+
+print(solveMeFirst(x: a, y: b))
