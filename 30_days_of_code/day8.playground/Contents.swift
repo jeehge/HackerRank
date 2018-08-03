b05cdb83c20be913565a6f42eb211fd83255dce8
jihye <taerg89@gmail.com>
2018-08-03 09:09:13 +0900

    day update

--- 30_days_of_code/day8.playground/Contents.swift
+++ 30_days_of_code/day8.playground/Contents.swift
@@ -13,35 +13,37 @@ import Foundation
  이름을 입력했을 때 전화번호부에 존재하지 않는다면
  Not found 출력
  */
-// Dictionary (key, value)
-var phoneBook:Dictionary = [String:String]()
 
-// 전화번호 항목 수를 나타내는 정수
-guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
-    else { fatalError("Bad input") }
+// [String: String] -> Dictionary<String, String> 축약표현
+var phoneBook = Dictionary<String,String>()
 
-// 제약조건 1 <= n <= 100000
-if n>=1 && n<=100000 {
-    // 입력받은 값을 저장
+// n : 전화번호 항목 수를 나타내는 정수
+if let input = readLine(), let n = Int(input) {
     for _ in 1...n {
-        guard let inputValue = readLine() else { fatalError("Bad input") }
-        let info = inputValue.split(separator: " ").map{(value)-> String in
-            return String(value)
+        if let input = readLine() {
+            let info = input.split(separator: " ").map(String.init)
+            let name = info[0]
+            let phoneNumber = info[1]
+            phoneBook[name] = phoneNumber
         }
-        // Dictionary에 값 추가
-        phoneBook.updateValue(info[1], forKey: info[0])
     }
-    // key 값 입력받아 비교
-    for i in 0...n-1 {
-        guard let inputName:String = readLine() else { fatalError("Bad input") }
-        var keys:[String] = phoneBook.map { (tuple:(String, String)) -> String in
-            return tuple.0
-        }
-        if inputName == keys[i] {
-            print("\(inputName)=\(phoneBook[inputName]!)")
-        } else {
-            print("Not found")
-        }
+    
+    if let inputName = readLine() {
+        var name = String(inputName)
         
+        // do ~ while
+        repeat {
+            if let phoneNumber = phoneBook[name] {
+                print("\(name)=\(phoneNumber)")
+            } else {
+                print("Not found")
+            }
+            
+            if let inputName = readLine() {
+                name = String(inputName)
+            } else {
+                name = ""
+            }
+        } while name.count > 0
     }
 }
